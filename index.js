0a0401ebd956c7565217fa7b54ce8866614e4316
Maraah Sang <maraahsang@gmail.com>
2024-01-08 18:27:58 +0000

    Randfomly return Rock Paper Scissors

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-
-
-let arr = ["rock", "paper", "scissors"]
-
-
+let pick = ["rock", "paper", "scissors"]
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random()*3)
-    return arr[randomNumber]
+  let randomPlay = Math.floor(Math.random()*pick.length)
+  return pick[randomPlay]
 }
 console.log(getComputerChoice())
-
-function playRound(playerSelection, computerSelection) {
-
-}
-
-const playerSelection = "rock";
- const computerSelection = getComputerChoice();
- console.log(playRound(playerSelection, computerSelection));
-
-
-// let arr = [rock, paper, scissors]
-// function playRound(playerSelection, getComputerChoice) {
-// function getComputerChoice() {
-//     let rand = Math.floor(Math.random()*3) + 1
-//     return arr[rand]
-// }
-
-// } 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
