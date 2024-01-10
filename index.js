a5434dc60b257655ed4b4c84a8b24acce7bc6565
Maraah Sang <maraahsang@gmail.com>
2024-01-10 21:13:49 +0000

    Play single round of Rock Paper Scissors

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-let pick = ["rock", "paper", "scissors"]
+let gameArray = ["rock", "paper", "scissors"]
 function getComputerChoice() {
-  let randomPlay = Math.floor(Math.random()*pick.length)
-  return pick[randomPlay]
+  let randomPlay = Math.floor(Math.random()*gameArray.length)
+  return gameArray[randomPlay]
 }
 console.log(getComputerChoice())
+
+function playerRound(playerSelection, computerSelection) {
+  let result
+   if (playerSelection === 'rock' && computerSelection === 'paper') {
+    result = "You Lose ... 'Paper beats Rock'";
+    
+  } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+    result = "You Win!!! Rock beats the scissor";
+    
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    result = "Hurray you win!! Paper covers the rock";
+
+  } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+    result = "Oh no you lose.. Scissor cuts the paper";
+  
+  } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+    result = "You lose .. Rock beats the scissor";
+    
+  } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+    result = "You win!! Scissor cut the paper";
+    
+  } else {
+    result = "You entered wrong  choice";
+  }
+  return result;
+}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoice()
+console.log(playerRound(playerSelection, computerSelection));
