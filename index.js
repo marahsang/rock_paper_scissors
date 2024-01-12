48d31fdc24948a02013816e1eadf2590942f77df
Maraah Sang <maraahsang@gmail.com>
2024-01-12 15:33:37 +0000

    Write a function to play Best of Five game keeps Score and displays Result

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 
 console.log("Rock Paper Scissor Game");
-
+let computerScore = 0;
+let userScore = 0;
 function getComputerChoice() {
   let gameArray = ["rock", "paper", "scissor"];
   let randomPlay = Math.floor(Math.random() * gameArray.length);
@@ -12,33 +13,84 @@ function playerRound(playerSelection, computerSelection)  {
   let result;
   if (playerSelection === computerSelection) {
     result = "Draw Match";
+    result += '\n'
+    result += " Your Score: " + userScore
+    result += '\n'
+    result += " Computer Score: " + computerScore
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
     
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
     result = "You Lose ... 'Paper beats Rock'";
+    computerScore++;
+        result = "You Lose! Paper beats rock!"
+        result += '\n'
+        result += " Your Score: " + userScore
+        result += '\n'
+        result += " Computer Score: " + computerScore
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
   } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
     result = "You Win!!! Rock breaks the scissor";
+    computerScore++;
+         result = "You Lose! Paper beats rock!"
+        result += '\n'
+        result += " Your Score: " + userScore
+        result += '\n'
+        result += " Computer Score: " + computerScore
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     result = "Hurray you win!! Paper covers the rock";
+    computerScore++;
+         result = "You Lose! Paper beats rock!"
+        result += '\n'
+        result += " Your Score: " + userScore
+        result += '\n'
+        result += " Computer Score: " + computerScore
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
 
   } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
     result = "Oh no you lose.. Scissor cuts the paper";
+    computerScore++;
+        result = "You Lose! Paper beats rock!"
+        result += '\n'
+        result += " Your Score: " + userScore
+        result += '\n'
+        result += " Computer Score: " + computerScore
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
   } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
     result = "You lose .. Rock breaks the scissor";
+    computerScore++;
+        result = "You Lose! Paper beats rock!"
+        result += '\n'
+        result += " Your Score: " + userScore
+        result += '\n'
+        result += " Computer Score: " + computerScore
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
   } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
     result = "You win!! Scissor cut the paper";
+    computerScore++;
+        result = "You Lose! Paper beats rock!"
+        result += '\n'
+        result += " Your Score: " + userScore
+        result += '\n'
+        result += " Computer Score: " + computerScore
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
   } else {
     result = "You entered wrong  choice";
+    computerScore++;
+        result = "You Lose! Paper beats rock!"
+        result += '\n'
+        result += " Your Score: " + userScore
+        result += '\n'
+        result += " Computer Score: " + computerScore
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
   }
-  return result;
 }
-const playerSelection = prompt('Choose Rock, Paper, or Scissors').toLowerCase();
-const computerSelection = getComputerChoice()
-console.log(playerRound(playerSelection, computerSelection))
+
+function game() {
+    for (i = 0; i < 5; i++) {
+        const playerSelection = prompt('Choose Rock, Paper, or Scissors').toLowerCase();
+        const computerSelection = getComputerChoice()
+        console.log(playerRound(playerSelection, computerSelection))
+    }
+}
+game()
