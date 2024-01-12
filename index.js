876d88d2b86ba50ee531b7aff79a0ae8a54c786a
Maraah Sang <maraahsang@gmail.com>
2024-01-12 17:03:09 +0000

    Fix repeat gamein TIES occurrence

--- index.js
+++ index.js
@@ -1,80 +1,77 @@
 
 
 console.log("Rock Paper Scissor Game");
+
 let computerScore = 0;
 let userScore = 0;
+
 function getComputerChoice() {
   let gameArray = ["rock", "paper", "scissor"];
   let randomPlay = Math.floor(Math.random() * gameArray.length);
   return gameArray[randomPlay];
 }
 
-function playerRound(playerSelection, computerSelection)  {
-  let result;
-  if (playerSelection === computerSelection) {
-    result = "Draw Match";
+function playRound() {
+if (playerSelection ===  computerSelection) {
+    let result = "Tie! Try Again!"
+    result += '\n'
+    result += " Your Score: " + userScore
+    result += '\n'
+    result += " Computer Score: " + computerScore
+    return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
+} else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    computerScore++;
+    let result = "You Lose! Paper beats rock!"
+    result += '\n'
+    result += " Your Score: " + userScore
+    result += '\n'
+    result += " Computer Score: " + computerScore
+    return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
+} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    userScore++;
+    let result = "You Win! Rock beats scissors!"
+    result += '\n'
+    result += " Your Score: " + userScore
+    result += '\n'
+    result += " Computer Score: " + computerScore
+    return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
+
+} else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    userScore++;
+    let result = "You Win! Paper beats rock!"
+    result += '\n'
+    result += " Your Score: " + userScore
+    result += '\n'
+    result += " Computer Score: " + computerScore
+    return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
+} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    computerScore++;
+    let result = "You Lose! Scissors beats paper!"
+    result += '\n'
+    result += " Your Score: " + userScore
+    result += '\n'
+    result += " Computer Score: " + computerScore
+    return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);        
+} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    computerScore++;
+    let result = "You Lose! Rock beats scissors!";
     result += '\n'
     result += " Your Score: " + userScore
     result += '\n'
     result += " Computer Score: " + computerScore
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
     
-  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    result = "You Lose ... 'Paper beats Rock'";
-    computerScore++;
-        result = "You Lose! Paper beats rock!"
-        result += '\n'
-        result += " Your Score: " + userScore
-        result += '\n'
-        result += " Computer Score: " + computerScore
-    return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
-  } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
-    result = "You Win!!! Rock breaks the scissor";
-    computerScore++;
-         result = "You Lose! Paper beats rock!"
-        result += '\n'
-        result += " Your Score: " + userScore
-        result += '\n'
-        result += " Computer Score: " + computerScore
-    return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    result = "Hurray you win!! Paper covers the rock";
-    computerScore++;
-         result = "You Lose! Paper beats rock!"
-        result += '\n'
-        result += " Your Score: " + userScore
-        result += '\n'
-        result += " Computer Score: " + computerScore
+} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    userScore++;
+    let result = "You Win! Scissors beats paper!"
+    result += '\n'
+    result += " Your Score: " + userScore
+    result += '\n'
+    result += " Computer Score: " + computerScore
+    
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
 
-  } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
-    result = "Oh no you lose.. Scissor cuts the paper";
-    computerScore++;
-        result = "You Lose! Paper beats rock!"
-        result += '\n'
-        result += " Your Score: " + userScore
-        result += '\n'
-        result += " Computer Score: " + computerScore
-    return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
-  } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
-    result = "You lose .. Rock breaks the scissor";
-    computerScore++;
-        result = "You Lose! Paper beats rock!"
-        result += '\n'
-        result += " Your Score: " + userScore
-        result += '\n'
-        result += " Computer Score: " + computerScore
-    return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
-  } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
-    result = "You win!! Scissor cut the paper";
-    computerScore++;
-        result = "You Lose! Paper beats rock!"
-        result += '\n'
-        result += " Your Score: " + userScore
-        result += '\n'
-        result += " Computer Score: " + computerScore
-    return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
-  } else {
+} else {
     result = "You entered wrong  choice";
     computerScore++;
         result = "You Lose! Paper beats rock!"
@@ -83,7 +80,7 @@ function playerRound(playerSelection, computerSelection)  {
         result += '\n'
         result += " Computer Score: " + computerScore
     return(result + "\n" + "Your Choice: " + playerSelection + ". " + "Computer Choice: " + computerSelection);
-  }
+    }
 }
 
 function endGame() {
@@ -95,13 +92,13 @@ function endGame() {
 }
 
 function game() {
-    const playerSelection = prompt('Choose Rock, Paper, or Scissors').toLowerCase();
-        const computerSelection = getComputerChoice()
-        console.log(playerRound(playerSelection, computerSelection))
+    let playerSelection = prompt('Choose Rock, Paper, or Scissors').toLowerCase();
+        let computerSelection = getComputerChoice()
+        console.log(playRound(playerSelection, computerSelection))
     if(userScore < 5 && computerScore < 5){
     	game();
     }
-    else{
+    else {
     	endGame();
     }
 }
